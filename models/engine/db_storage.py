019ee9dfb01d71e219d6e4d03ec88798c28e13ef
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-19 06:19:22 -0800

    changes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,8 +3,11 @@
 import os
 from sqlalchemy import create_engine
 from models.base_model import Base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 
 class DBStorage:
+    """ DBStorage class """
     __engine = None
     __session = None
 
@@ -14,15 +17,29 @@ class DBStorage:
                 os.getenv("HBNB_MYSQL_USER"),
                 os.getenv("HBNB_MYSQL_PWD"),
                 os.getenv("HBNB_MYSQL_HOST"),
-                os.getenv("HBNB_MYSQL_DB"),
+                os.getenv("HBNB_MYSQL_DB")),
                 pool_pre_ping=True
-            )
         )
-        if os.getenv(HBNB_ENV) == "test":
+        if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
     
     def all(self, cls=None):
         """Query the DB"""
         if cls is not None:
-            return DBStorage.__session.query(cls).all()
-        return 
+            return self.__session.query(cls).all()
+        return self.__session.query().all()
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,7 @@ USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all priviledges on the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant select priviledge
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
