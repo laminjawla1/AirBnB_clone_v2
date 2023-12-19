9ee2117fb9524b7bf6a8080d4350c144463c3c6d
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-19 15:47:36 +0000

    DBStorage - Amenity and BOOM

--- models/user.py
+++ models/user.py
@@ -7,12 +7,17 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
 
-    places = relationship('Place', cascade='all, delete, delete-orphan', backref='user')
-    reviews = relationship('Review', cascade='all, delete, delete-orphan', backref='user')
+    places = relationship(
+        "Place", cascade="all, delete, delete-orphan", backref="user"
+    )
+    reviews = relationship(
+        "Review", cascade="all, delete, delete-orphan", backref="user"
+    )
