425b1f278c59b5d9e8700e05fadcc52a702f41eb
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-11 06:52:21 -0800

    Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,23 +1,30 @@
 # Setup the web servers for the deployment of web_static
 exec { '/usr/bin/env apt -y update' : }
+
 -> package { 'nginx':
   ensure => installed,
 }
+
 -> file { '/data':
   ensure  => 'directory'
 }
+
 -> file { '/data/web_static':
   ensure => 'directory'
 }
+
 -> file { '/data/web_static/releases':
   ensure => 'directory'
 }
+
 -> file { '/data/web_static/releases/test':
   ensure => 'directory'
 }
+
 -> file { '/data/web_static/shared':
   ensure => 'directory'
 }
+
 -> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "<!DOCTYPE html>
@@ -25,14 +32,16 @@ exec { '/usr/bin/env apt -y update' : }
   <head>
   </head>
   <body>
-    <p>Nginx server test</p>
+    <p>Testing Nginx</p>
   </body>
 </html>"
 }
+
 -> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
 }
+
 -> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
