2d97917b0e40838c713a4ea34bac12b390f79f66
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-11 00:26:33 -0800

    Prepare your webserver- refactored

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,24 @@
 #!/usr/bin/env bash
-# Sets up the web server for the deployment of web_static
+# A script to setup a web server for deployment.
 
-# Update and upgrade packages
+# Update and upgrade the packages
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install nginx if not exists
-sudo apt-get install nginx
+# Install nginx
+sudo apt-get install nginx -y
 
-# Create the required folders
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
-sudo echo "<h1>Some Dummy Heading</h1>" | tee /data/web_static/releases/test/index.html
-
-# Delete the symbolic link if it exists else handle the exception
-# and create the actual symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Ownership and group to ubuntu recursive
-sudo chown -R ubuntu:ubuntu /data
-
-# Update the Nginx confuguration to server the content of
-# /data/web_static/ to hbnb_static
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-
-# Restart the nginx service
 sudo service nginx restart
