4773d5d35f69dc627c83a6d261417a2c72101512
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-11 01:19:54 -0800

    Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,24 +4,22 @@ Compress all files fom web_static directory, generates a .tgz archive file
 and deploy the archive file to a remote server
 """
 import time
-from fabric.api import local, run, put
+from fabric.api import local, run, put, env
 
+
+env.hosts = ["54.160.88.197", "52.91.178.63"]
+env.user = "ubuntu"
 
 def do_pack():
     """
     Pack all all files in web_static directory into an archive file
     """
-    try:
-        local("mkdir -p versions")
-        local(
-            "tar -cvzf versions/web_static_{}.tgz web_static/".format(
-                time.strftime("%Y%m%d%H%M%S")
-            )
-        )
-        return "versions/web_static_{}.tgz".format(
-                time.strftime("%Y%m%d%H%M%S"))
-    except Exception as e:
-        return None
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    return archived_f_path if t_gzip_archive.succeeded else None
 
 
 def do_deploy(archive_path):
@@ -45,7 +43,7 @@ def do_deploy(archive_path):
         run("sudo ln -s {} /data/web_static/current".format(
             newest_version))
 
-        print("Version {} is succesfully deployed!".format(version))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
