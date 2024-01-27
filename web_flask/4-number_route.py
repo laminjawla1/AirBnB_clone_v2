944b6bffca0333356411f1f56f7d448635271ac0
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-27 12:07:36 +0000

    is numsber

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This flask script sends a response with the message "Hello HBNB!"
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def helloHBNB():
+    """Send a message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def is_fun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", strict_slashes=False, defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
