4e8badf54d7a4fc95cc1e88bea6916e9a1b3d1b2
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-27 11:34:51 +0000

    HNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This flask script sends a response with the message "Hello HBNB!"
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def helloHBNB():
+    """Send a message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
